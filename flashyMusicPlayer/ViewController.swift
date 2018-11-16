2f9042898122bdfa0da417a12e161ff11c69fdcc
Eugene Myroniuk <a01005177@labs-imac-2.ad.bcit.ca>
2018-11-16 10:45:24 -0800

    Initial Commit

---
+++ flashyMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flashyMusicPlayer
+//
+//  Created by Eugene Myroniuk on 2018-11-16.
+//  Copyright © 2018 Eugene Myroniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
